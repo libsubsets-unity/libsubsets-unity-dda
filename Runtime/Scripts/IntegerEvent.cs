e8c0d52f8b9175883de437c05adabad6d5868239
Lee, Hyeon-gi <maaya.lee@gmail.com>
2022-07-15 21:04:41 +0900

    사운드 추가 및 이벤트 처리타입 추가

---
+++ Runtime/Scripts/IntegerEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Subsets.Message2
+{
+    [CreateAssetMenu]
+    public class IntegerEvent : GameEvent
+    {
+        public int Variable;
+        
+        public void Raise(int variable)
+        {
+            this.Variable = variable;
+            for(int i = eventListeners.Count -1; i >= 0; i--)
+                eventListeners[i].OnEventRaised();
+        }
+    }
+}
---
+++ Runtime/Scripts/IntegerEventListener.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Subsets.Message2 
+{
+    public class IntegerEventListener : GameEventListener
+    {
+        public int RaiseCondition = 0;
+        public override void OnEventRaised()
+        {
+            IntegerEvent e = Event as IntegerEvent;
+            if (e)
+            {
+                if (e.Variable == RaiseCondition)
+                {
+                    Response.Invoke();
+                }
+            }
+        }
+    }
+}
