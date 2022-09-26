b72682a15eee2202569d6342255bd10c0742f9c2
Lee, Hyeon-gi <maayalee@gmail.com>
2022-09-27 08:38:16 +0900

    Tets

---
+++ Runtime/Scripts/TestGameEventListener.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+namespace Subsets.Message2
+{
+    public class TestGameEventListener : MonoBehaviour
+    {
+        public string ViewEventName;
+        
+        public UnityEvent Listeners;
+        public UnityEvent<int> Listeners2;
+
+        public void Awake()
+        {
+            EventTrigger trigger = GetComponent<EventTrigger>();
+        }
+
+        public void Raise()
+        {
+            /*
+            if (Listeners != null)
+            {
+                Listeners.Invoke();
+            }
+            */
+        }
+
+        public void TestMethod(int param)
+        {
+            Debug.Log(param);
+        }
+    }
+}
