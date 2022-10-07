77fe53b6891c1174da187fdb96f1f2da5ec4b177
Lee, Hyeon-gi <maaya.lee@gmail.com>
2022-10-08 00:25:18 +0900

    Added menu command

---
+++ Runtime/Scripts/GameObjectEvent.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Subsets.Message2 
+{
+    [CreateAssetMenu]
+    public class GameObjectEvent : BaseEvent<GameObject>
+    {
+    }
+}
