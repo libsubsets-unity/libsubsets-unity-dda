d5d75cf7a1f028ee3a2f26b9670d19e7ab70c256
Lee, Hyeon-gi <maayalee@gmail.com>
2022-11-10 10:40:36 +0900

    Change border ordering and size, modified ad callback

---
+++ Editor/Scripts/BoolVariableEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+using Subsets.Message2.Runtime;
+
+namespace Subsets.Message2.Editor
+{
+    [CustomEditor(typeof(BoolVariable), editorForChildClasses: true)]
+    public class BoolVariableEditor : UnityEditor.Editor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+
+            GUI.enabled = Application.isPlaying;
+
+            if (GUILayout.Button("Raise Change Event"))
+            {
+                BoolVariable e = target as BoolVariable;
+                if (e)
+                {
+                    e.Value = e.Value;
+                }
+            }
+        }
+    }
+}
