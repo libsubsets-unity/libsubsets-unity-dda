c77fc2798975f1ad5685883e9a80849383485373
Lee, Hyeon-gi <maaya.lee@gmail.com>
2022-12-12 18:02:12 +0900

    Added key value trigger

---
+++ Runtime/Scripts/BoolKeyValueVariableTrigger.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Subsets.Message2.Runtime
+{
+    public class BoolKeyValueVariableTrigger : MonoBehaviour
+    {
+        public string Id;
+        public BoolKeyValueVariable Variable;
+        public BoolCondition Condition;
+        public bool StopWhenConditionMatched;
+        public UnityEvent<BoolVariable> Listeners;
+       
+        private void OnEnable()
+        {
+        }
+        public void Awake()
+        {
+            Variable.Find(Id).PropertyChanged += delegate(object sender, PropertyChangedEventArgs args)
+            {
+                Execute();
+            };
+        }
+
+        private void Execute()
+        {
+            if (enabled)
+            {
+                if (Condition.Compare == BoolCompare.IsTrue)
+                {
+                    if (Variable.Find(Id).Value == true)
+                    {
+                        Listeners.Invoke(Variable.Find(Id));
+                        if (StopWhenConditionMatched)
+                        {
+                            enabled = false;
+                        }
+                    }
+                }
+                else if (Condition.Compare == BoolCompare.IsFalse)
+                {
+                    if (Variable.Find(Id).Value == false)
+                    {
+                        Listeners.Invoke(Variable.Find(Id));
+                        if (StopWhenConditionMatched)
+                        {
+                            enabled = false;
+                        }
+                    }
+                }
+            }           
+        }
+    }
+}
